263835984e2ddf9eacf0626510d32cb9fed0dc2d
James Riggleman <james.riggleman@yahoo.com>
2020-12-01 21:48:27 +0800

    first commit

---
+++ pages/index/popup.js
@@ -0,0 +1,23 @@
+// pages/index/popup.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
